c7f394e98fd820c41622afa329d8581e439ede15
harsha varthan <66173033+rpharshavarthan@users.noreply.github.com>
2023-10-31 08:42:34 +0530

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, Express.js Server!');
+  // Serve the HTML file
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/.well-known/acme-challenge/', (req, res) => {
+  const filename = req.params.filename;
+  const challengeData = "4RnfWtLKaIc6EdhsOsr4fb6RFZuUoabZW5dPW36cmc";
+  res.send(challengeData);
 });
 
 app.listen(port, () => {
